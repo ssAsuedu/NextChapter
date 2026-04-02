81e2917c660022ecff2429fc4a5fc5f277773310
tasnumi <tasnimsgames@gmail.com>
2026-04-01 22:17:57 -0700

    feat: handle account page error states
    added error handling to the account info password form
    
    added better responsive styles to the page
    
    added loading states on the buttons depending on if its saving or not

--- client/src/pages/ProfileSubPages/Account.jsx
+++ client/src/pages/ProfileSubPages/Account.jsx
@@ -10,18 +10,70 @@ const Account = () => {
   const email = localStorage.getItem("userEmail");
   const [user, setUser] = useState(null);
   const [friendCount, setFriendCount] = useState(0);
-  const [editing, setEditing] = useState(false);
+  const [usernameUpdated, setUsernameUpdated] = useState("");
   const [newName, setNewName] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [userNameLoading, setUsernameLoading] = useState(false);
+  const [passwordLoading, setPasswordLoading] = useState(false);
 
   // Password change state
   const [showPasswordModal, setShowPasswordModal] = useState(false);
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [passwordError, setPasswordError] = useState([]);
+  const [passwordError, setPasswordError] = useState({});
   const [passwordSuccess, setPasswordSuccess] = useState();
+  const [emptyInputs, setEmptyInputs] = useState(false);
 
+  const validatePassword = (oldPassword, newPassword, confirmPassword) => {
+    const errors = {
+      length: "",
+      uppercase: "", 
+      number: "",
+      special: "",
+      newPass: "",
+      currPass: "",
+    };
+
+    const specialSymbols = "!@#$%^&*";
+    if (!oldPassword || !newPassword || !confirmPassword) {
+      setShowPasswordModal(true); // show modal when there are errors
+      setOldPassword("");
+      setNewPassword("");
+      setConfirmPassword("");
+      errors.fields = "All fields are required."; //first error type is all fields are required
+    }
+
+    if (newPassword !== confirmPassword) {
+      setShowPasswordModal(true); // show modal when there are errors
+      setOldPassword("");
+      setNewPassword("");
+      setConfirmPassword("");  
+      errors.newPass = "New passwords do not match."; //second error type is passwords dont match
+      
+    }
+    if (newPassword.length < 8) {
+      setShowPasswordModal(true); // show modal when there are errors
+      setOldPassword("");
+      setNewPassword("");
+      setConfirmPassword("");  
+      errors.length = "Password must be at least 8 characters."; //third error stype is that password must be 8 characters at least
+      
+    }
+    if (!/[A-Z]/.test(newPassword)) {
+      errors.uppercase = "Password must include at least one uppercase letter.";
+    }
+    
+    if (!/[0-9]/.test(newPassword)) {
+      errors.number = "Password must include at least one number.";
+    }
+
+    if(!specialSymbols.split("").some(symbol => newPassword.includes(symbol))) {
+      errors.special = "Password must include at least one special character !@#$%^&*.";
+    }
+
+    return errors;
+  };
+  
   useEffect(() => {
     const fetchUserData = async () => {
       if (!email) return;
@@ -41,89 +93,66 @@ const Account = () => {
   }, [email]);
 
   const handleNameChange = async () => {
-    if (!newName.trim() || !user) return;
-    setLoading(true);
+    if (!newName.trim() || newName.trim() === user.name) {
+      console.log("hello");
+      setEmptyInputs(true);
+      return;
+    }
     try {
+      setUsernameLoading(true);
       await updateUserName({ email: user.email, name: newName.trim() });
       setUser({ ...user, name: newName.trim() });
       localStorage.setItem("userName", newName.trim());
-      setEditing(false);
+      setUsernameUpdated("Username successfully updated.");
+      
     } catch (err) {
-      alert("Failed to update name.");
+      // alert("Failed to update name.");
+      setEmptyInputs(true);
     }
-    setLoading(false);
+    setTimeout(() => {
+        setUsernameUpdated("");
+      }, 3000);
+    setUsernameLoading(false);
   };
 
   // Password change handler
   const handleChangePassword = async (e) => {
     e.preventDefault();
-    setPasswordError([]);
+    setPasswordError({});
     setPasswordSuccess("");
-    const errorsList = []; //create an array that stores every possible error type
-    const specialSymbols = "!@#$%^&*"; //symbols that are acceptable in a password
-
-    if (!oldPassword || !newPassword || !confirmPassword) {
-      setShowPasswordModal(true); // show modal when there are errors
-      setOldPassword("");
-      setNewPassword("");
-      setConfirmPassword("");
-      errorsList[0] = "All fields are required."; //first error type is all fields are required
-      
-      
-    }
-    if (newPassword !== confirmPassword) {
-      setShowPasswordModal(true); // show modal when there are errors
-      setOldPassword("");
-      setNewPassword("");
-      setConfirmPassword("");  
-      errorsList[1] = "New passwords do not match."; //second error type is passwords dont match
-      
-    }
-    if (newPassword.length < 8) {
-      setShowPasswordModal(true); // show modal when there are errors
-      setOldPassword("");
-      setNewPassword("");
-      setConfirmPassword("");  
-      errorsList[2] = "Password must be at least 8 characters."; //third error stype is that password must be 8 characters at least
-      
-    }
-    if (!/[A-Z]/.test(newPassword)) {
-      errorsList[3] = "Password must include at least one uppercase letter.";
-    }
+    const errorsList =  validatePassword(oldPassword, newPassword, confirmPassword)
     
-    if (!/[0-9]/.test(newPassword)) {
-      errorsList[4] = "Password must include at least one number.";
-    }
-
-    if(!specialSymbols.split("").some(symbol => newPassword.includes(symbol))) {
-      errorsList[5] = "Password must include at least one special character !@#$%^&*.";
-    }
-
-    if (errorsList.length > 0) {
-      setPasswordError(errorsList);
-      setShowPasswordModal(true);
-      return;
-    }
     try {
       await changePassword({ email, oldPassword, newPassword }); // Use the API function here
+      setPasswordLoading(true);
       setShowPasswordModal(true); // show modal when there are errors
       setOldPassword("");
       setNewPassword("");
       setConfirmPassword("");
       setPasswordSuccess("Password changed successfully!");
-      setPasswordError([]);
+      
     } catch (err) {
+      if(err.response?.status === 401) {
+        console.log("hello");
+        errorsList.currPass = "Current password is incorrect.";
+        setPasswordLoading(false);
+      }
       setShowPasswordModal(true);
       setOldPassword("");
       setNewPassword("");
-      setConfirmPassword("");    
+      setConfirmPassword("");  
+      setPasswordLoading(false);  
       const apiError = err.response?.data?.error || "Failed to change password.";
-      setPasswordError(Array.isArray(apiError) ? apiError: [apiError]);
     }
+    setPasswordError(errorsList);
 
     if (passwordSuccess != " ") {
       setShowPasswordModal(true);
     }
+    setTimeout(() => {
+        setPasswordSuccess("");
+      }, 3000);
+      
   };
 
   if (!user) {
@@ -143,12 +172,14 @@ const Account = () => {
       <ProfileNavbar />
       <div className="account-info">
         <div className="info-display">
-          <img className="profile-logo account-img" src={profileImage} alt="Profile Icon"></img>
-          <div className="name-email-wrapper">
-            <p className="account-user"><strong>{user.name}</strong></p>
-            <p className="account-secondary email-text">{email}</p>
-            <p className="account-secondary member-text"><strong>Member Since:</strong> <span style={{ marginLeft: 0 }}>{user.createdAt ? new Date(user.createdAt).toLocaleDateString() : "N/A"}</span></p>
-            <p className="account-secondary friend-text"><strong>Number of Friends:</strong> <span style={{ marginLeft: 0 }}>{friendCount}</span></p>
+          <div className="info-items">
+            <img className="profile-logo account-img" src={profileImage} alt="Profile Icon"></img>
+            <div className="name-email-wrapper">
+              <p className="account-user"><strong>{user.name}</strong></p>
+              <p className="account-secondary email-text">{email}</p>
+              <p className="account-secondary member-text"><strong>Member Since:</strong> <span style={{ marginLeft: 0 }}>{user.createdAt ? new Date(user.createdAt).toLocaleDateString() : "N/A"}</span></p>
+              <p className="account-secondary friend-text"><strong>Number of Friends:</strong> <span style={{ marginLeft: 0 }}>{friendCount}</span></p>
+            </div>
           </div>
         </div>
         
@@ -158,21 +189,29 @@ const Account = () => {
           <h2>Name</h2>
           <TextField className="name-input"
             placeholder="Enter new name"
-            // value={newName}
-            onChange={e => setNewName(e.target.value)}
+            onChange={e => {
+              setNewName(e.target.value)
+              setEmptyInputs(false);
+            }}
             variant="outlined"
           />
         </div>
+        {emptyInputs && (
+          <div className="error-styles">
+            <p>Please enter a new name.</p>
+            </div>
+        )}
+        {usernameUpdated && (
+          <span className="success-styles">{usernameUpdated}</span>
+        )}
         <hr className="account-separator" />
         
           <div className="account-info-buttons">
             <button className="signup-button change-password" onClick={() => setShowPasswordModal(true)}>
             Change Password
           </button>
-          <button className="browse-button save-changes" onClick={(handleNameChange)}>Save Changes</button>
+          <button className="browse-button save-changes" onClick={(handleNameChange)} disabled={userNameLoading}>{userNameLoading ? "Saving..." : "Save Changes"}</button>
           </div>
-        
-        {/* {passwordError && <div className="error-styles">{passwordError}</div>} */}
         
         {showPasswordModal && (
           <div className="password-modal">
@@ -187,10 +226,10 @@ const Account = () => {
                     type="password"
                     value={oldPassword}
                     onChange={e => setOldPassword(e.target.value)}
-                    
-                    // error={!!passwordError}
                   />
-                  {/* {passwordError && <div className="error-styles">{passwordError}</div>} */}
+                  <div className="error-styles">
+                    {passwordError?.currPass && <p>{passwordError.currPass}</p>}
+                  </div>
                 </div>
                 <div className="label-and-input">
                   <label className="input-label">
@@ -199,12 +238,43 @@ const Account = () => {
                     <input className="password-input"
                       type="password"
                       value={newPassword}
-                      onChange={e => setNewPassword(e.target.value)}
-                      // error={!!passwordError}
+                      onChange={e => {
+                        setNewPassword(e.target.value)
+                        if(passwordError.length) {
+                          setPasswordError((prev) => ({
+                            ...prev,
+                            length: "",
+                          }));
+                        };
+                        if(passwordError.uppercase) {
+                          setPasswordError((prev) => ({
+                            ...prev,
+                            uppercase: "",
+                          }));
+                        };
+                        if(passwordError.number) {
+                          setPasswordError((prev) => ({
+                            ...prev,
+                            number: "",
+                          }));
+                        };
+                        if(passwordError.special) {
+                          setPasswordError((prev) => ({
+                            ...prev,
+                            special: "",
+                          }));
+                        };
+                      }}
                     />
-                    {passwordError.slice(2,6).map((err, idx) => ( //splits the array from indices 2-5, map creates a span for every error
-                      err ? <span key={idx} className="error-styles">{err}</span> : null //if an error exists, create a span for that indices error and display it. otherwise, don't display it. 
-                    ))}
+                    {passwordError && (
+                      <div className="error-styles">
+                      {passwordError.length != "" && <p>{passwordError.length}</p>}
+                      {passwordError.uppercase && <p>{passwordError.uppercase}</p>}
+                      {passwordError.number && <p>{passwordError.number}</p>}
+                      {passwordError.special && <p>{passwordError.special}</p>}
+                    </div>
+                    )}
+                    
                 </div>
                 
                 <div className="label-and-input">
@@ -215,11 +285,12 @@ const Account = () => {
                       type="password"
                       value={confirmPassword}
                       onChange={e => setConfirmPassword(e.target.value)}
-                      // error={!!passwordError}
                     />
-                    {/* {passwordError && <div className="error-styles">{passwordError}</div>} */}
+                    {passwordError.newPass && <div className="error-styles">{passwordError.newPass}</div>}
                 </div>
-                <span className="error-styles">{passwordError[0]}</span>
+                <div className="error-styles">
+                  {passwordError.fields && <p>{passwordError.fields}</p>}
+                </div>
                 {passwordSuccess && (<span className="success-styles">{passwordSuccess}</span>)}
               </div>
               
@@ -234,7 +305,7 @@ const Account = () => {
                       setOldPassword("");
                       setNewPassword("");
                       setConfirmPassword("");
-                      setPasswordError([]);
+                      setPasswordError({});
                       setPasswordSuccess("");
                     }}
                     >
