15953c42b0c0226ad83c610320819510e0b77e39
tasnumi <tasnimsgames@gmail.com>
2026-04-13 21:31:21 -0700

    feat: change text if logged in/signed out
    if the user is logged in, removed the get started button + log in text
    
    if the user is not logged in, keep the get started button + log in text
    
    fixed hover state on navbar for signup button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,15 @@ import "../styles/LandingPage/Home.css";
 import { useNavigate } from "react-router-dom";
 import ProfileSVG from "../assets/profile.svg";
 import Community from "../assets/community.svg";
-import Team from '@mui/icons-material/Groups';
-import Values from '@mui/icons-material/VolunteerActivism';
-import Book from '@mui/icons-material/AutoStories';
-import DoneAllIcon from '@mui/icons-material/DoneAll';
+import Team from "@mui/icons-material/Groups";
+import Values from "@mui/icons-material/VolunteerActivism";
+import Book from "@mui/icons-material/AutoStories";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 const Home = () => {
   const navigate = useNavigate();
+
+  const token = localStorage.getItem("token"); //check to see if user is logged in
+  const isAuthenticated = !!token; //true -> user is logged in. false -> user is not logged in
 
   return (
     <div className="landing-page">
@@ -18,13 +21,44 @@ const Home = () => {
           <div className="left-side-hero">
             <h1 id="hero-heading">Discover Worlds Between Pages</h1>
             <div className="hero-list">
-              <p><DoneAllIcon aria-hidden="true" /> Explore New Books and Connect with Friends</p>
-              <p><DoneAllIcon aria-hidden="true" /> Track Your Progress and Earn Badges</p>
-              <p><DoneAllIcon aria-hidden="true" /> Leave Ratings and View Recommendations</p>
+              <p>
+                <DoneAllIcon aria-hidden="true" /> Explore New Books and Connect
+                with Friends
+              </p>
+              <p>
+                <DoneAllIcon aria-hidden="true" /> Track Your Progress and Earn
+                Badges
+              </p>
+              <p>
+                <DoneAllIcon aria-hidden="true" /> Leave Ratings and View
+                Recommendations
+              </p>
             </div>
             <div className="group-buttons">
-              <button className="browse-button" onClick={() => navigate("/explore")}  aria-label="Browse books on the Explore page">Browse Books</button>
-              <button className="signup-button" onClick={() => navigate("/signup") } aria-label="Sign up for a Next Chapter account">Get Started</button>
+              <button
+                className="browse-button"
+                onClick={() => navigate("/explore")}
+                aria-label="Browse books on the Explore page"
+              >
+                Browse Books
+              </button>
+              {!isAuthenticated ? (
+                <button
+                  className="signup-button"
+                  onClick={() => navigate("/signup")}
+                  aria-label="Sign up for a Next Chapter account"
+                >
+                  Get Started
+                </button>
+              ) : (
+                <button
+                  className="signup-button"
+                  onClick={() => navigate("/profile")}
+                  aria-label="Go to Profile to view your books"
+                >
+                  Your Library
+                </button>
+              )}
             </div>
           </div>
           <div className="right-side-hero">
@@ -52,50 +86,92 @@ const Home = () => {
           <div className="trending-books-right">
             <h2>Books Everyone's Talking About</h2>
             <p>
-              Discover the most popular books in our community. See what other readers are enjoying and join the conversation!
+              Discover the most popular books in our community. See what other
+              readers are enjoying and join the conversation!
             </p>
-            <button className="trending-books-button" onClick={() => navigate("/explore")}  aria-label="Explore trending books on the Explore page">Explore Trending Books</button>
+            <button
+              className="trending-books-button"
+              onClick={() => navigate("/explore")}
+              aria-label="Explore trending books on the Explore page"
+            >
+              Explore Trending Books
+            </button>
           </div>
         </div>
       </section>
       {/* About Section */}
       <section className="about">
         <div className="about-left">
-          <h3 className="values-title">Learn More About Next Chapter's Mission</h3>
+          <h3 className="values-title">
+            Learn More About Next Chapter's Mission
+          </h3>
         </div>
         <div className="about-wrapper">
-          <button className="about-button" aria-label="Learn more about the Next Chapter platform">
+          <button
+            className="about-button"
+            aria-label="Learn more about the Next Chapter platform"
+          >
             <Book className="about-icon" />
             Platform
-            <h6 className="about-card-text">Our application empowers users to achieve their reading goals.</h6>
+            <h6 className="about-card-text">
+              Our application empowers users to achieve their reading goals.
+            </h6>
             <div className="card-line"></div>
           </button>
-          
-          <button className="about-button" aria-label="Learn more about the team behind Next Chapter">
+
+          <button
+            className="about-button"
+            aria-label="Learn more about the team behind Next Chapter"
+          >
             <Team className="about-icon" />
             Team
-            <h6 className="about-card-text">Developed by a team of passionate readers who want to share their love for books.</h6>
+            <h6 className="about-card-text">
+              Developed by a team of passionate readers who want to share their
+              love for books.
+            </h6>
             <div className="card-line"></div>
           </button>
-          <button className="about-button" aria-label="Learn more about the core values that drive the Next Chapter platform">
+          <button
+            className="about-button"
+            aria-label="Learn more about the core values that drive the Next Chapter platform"
+          >
             <Values className="about-icon" />
             Values
-            <h6 className="about-card-text">Next Chapter is built on our core values of curiosity, connection, and self-improvement.</h6>
+            <h6 className="about-card-text">
+              Next Chapter is built on our core values of curiosity, connection,
+              and self-improvement.
+            </h6>
             <div className="card-line"></div>
           </button>
         </div>
-        
       </section>
 
       {/* Recommended Books Section */}
-      <section className="recommended-books"   aria-labelledby="recommended-heading">
+      <section
+        className="recommended-books"
+        aria-labelledby="recommended-heading"
+      >
         <h4 id="recommended-heading">Books We Think You Might Love</h4>
-        <p className="login-text">
-          Please <a href="/login" aria-label="Log in to your Next Chapter account">login</a> to see a collection of books we've selected that we think you'll enjoy!
-        </p>
+        {isAuthenticated ? (
+          <p className="login-text">
+            Check out the{" "}
+            <a href="/explore" aria-label="Check out the Explore Page">
+              Explore page{" "}
+            </a>
+            to discover books loved by other readers and start your next
+            chapter.
+          </p>
+        ) : (
+          <p className="login-text">
+            Please{" "}
+            <a href="/login" aria-label="Log in to your Next Chapter account">
+              login{" "}
+            </a>
+            to see a collection of books we've selected that we think you'll
+            enjoy!
+          </p>
+        )}
       </section>
-
-
     </div>
   );
 };
